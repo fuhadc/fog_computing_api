0830fcadaa9fa9b0c2f54e73edc524f646f2b9c4
fuhadc <fuhadcs@gmail.com>
2023-08-13 10:23:59 +0530

    fog_computing_api

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, abort
+from flask import Flask, abort ,request
 from flask_restful import Api, Resource, reqparse
 from pymongo import MongoClient
 import random
 import string
 import config
 from cryptography.fernet import Fernet
+import os
 
 app = Flask(__name__)
 api = Api(app)
@@ -18,6 +19,18 @@ users = db.users
 
 encryption_key = Fernet.generate_key()
 cipher_suite = Fernet(encryption_key)
+
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
+
+# Define the allowed file extensions
+ALLOWED_EXTENSIONS = {'csv'}
+
+# Function to check if the file has an allowed extension
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 class UserResource(Resource):
     def post(self):
@@ -95,7 +108,39 @@ class Get_Data(Resource):
         decrypted_data = cipher_suite.decrypt(encrypted_data).decode()
 
         return {'data': decrypted_data}, 200
-    
+class CSVUploadResource(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('api_key', type=str, required=True, help='API key is required')
+        args = parser.parse_args()
+
+        api_key = args['api_key']
+
+        # Check if API key exists in the database
+        user = users.find_one({'api_key': api_key})
+        if user is None:
+            abort(401, message='Invalid API key')
+
+        # Check if the POST request contains a file named 'csv_file'
+        if 'csv_file' not in request.files:
+            return {'message': 'No CSV file provided'}, 400
+
+        csv_file = request.files['csv_file']
+
+        if csv_file and allowed_file(csv_file.filename):
+            # Define the path based on the provided structure: data/api_key/name.csv
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], 'data', api_key, csv_file.filename)
+
+            # Create the directory if it doesn't exist
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+            # Save the uploaded CSV file, overwriting if the file already exists
+            csv_file.save(filename)
+            return {'message': 'CSV file uploaded successfully'}, 201
+        else:
+            return {'message': 'Invalid file format. Only CSV files are allowed.'}, 400
+
+api.add_resource(CSVUploadResource, '/csv_upload')
 api.add_resource(DataResource, '/send_data')
 api.add_resource(Get_Data, '/get_data/<string:data_key>')
 api.add_resource(UserResource, '/users')
